0d288a246b5b99f5dd78a152a74cf3fd5942b8c7
Edgardo Gabriel Aguirre <aguirre5411@gmail.com>
2023-05-21 20:23:13 -0300

    Clase 4 completada con exito.

--- vue04.js
+++ vue04.js
@@ -11,6 +11,7 @@ Vue.component('codermeals-card', {
                             <br><br>
                             <p class="text-black"> {{ titulo }} </p>
                             <p>$<strong> {{ costo }} </strong></p>
+                            <buttom class="btn btn-success btn-rounded">Agregar</button>
                     </div>
                 </div>`,
 })
@@ -19,9 +20,33 @@ Vue.component('codermeals-card', {
 var app = new Vue({
     el: '#app',
     data: {
-        titulo: "Pizza Napolitana",
-        portada: "https://img.freepik.com/vector-premium/pequena-ilustracion-animales-dibujos-animados-calamar_7814-741.jpg",
-        costo: 675.00
+        codermeals: [
+            {
+                id: 1,
+                titulo: "Spaghetti alla puttanesca",
+                costo: 575.00,
+                portada: "https://4.bp.blogspot.com/-D5Wvi_gX_Kg/WLatk_GVnKI/AAAAAAAAA5M/-y0gB26R0Dkb01QmdgfZqQALtd9NMV2DACLcB/s1600/P70301-072354.jpg"
+            },
+            {
+                id: 2,
+                titulo: "Pizza Napoletana al carciofi",
+                costo: 675.00,
+                portada: "https://static.cookist.it/wp-content/uploads/sites/21/2017/12/istock-480277738.jpg"
+            },
+            {
+                id: 3,
+                titulo: "Porchetta umbra a cottura lunga",
+                costo: 845.00,
+                portada: "https://www.fontecesia.it/wp-content/uploads/2018/11/porchetta-umbra-1920x1280.jpg"
+            },
+            {
+                id: 4,
+                titulo: "Orecchiette alle cime di rapa",
+                costo: 845.00,
+                portada: "https://irepo.primecp.com/2016/03/259860/recipe-8673_ExtraLarge1000_ID-1461628.jpg"
+            }
+        ]
+
 
     },
     methods: {

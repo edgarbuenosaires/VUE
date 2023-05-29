6afebff7bf621ca1a41bc8aaca57a168212ddbfe
Edgardo Gabriel Aguirre <aguirre5411@gmail.com>
2023-05-29 18:16:06 -0300

    Se agrega filtro

--- clase07/aplicacion/src/components/ParentComponent.vue
+++ clase07/aplicacion/src/components/ParentComponent.vue
@@ -3,9 +3,26 @@
     <h1>Componente Padre</h1>
     <p>Contador del componente padre: {{ contadorPadre }}</p>
     <p>Contador del componente hijo: {{ contadorHijo }}</p>
-    <button @click="incrementarContadorPadre">Incrementar contador del padre</button>
+    <button @click="incrementarContadorPadre">
+      Incrementar contador del padre
+    </button>
+
     <hr />
-    <child-component :contadorProp="contadorPadre" @contadorCambiado="actualizarContadorHijo"></child-component>
+
+    <child-component
+      :contadorProp="contadorPadre"
+      @contadorCambiado="actualizarContadorHijo"
+    ></child-component>
+
+    <hr />
+
+    <h2>Se aplica filto texto en mayusculas</h2>
+    <p>{{ texto | mayusculas }}</p>
+
+    <hr />
+
+    <h3>Se muestra el doble de 5</h3>
+    <p>{{ numero | doble }}</p>
   </div>
 </template>
 
@@ -20,7 +37,18 @@ export default {
     return {
       contadorPadre: 0,
       contadorHijo: 0,
+      texto: "texto escrito en minuscula",
+      numero: 5,
     };
+  },
+  filters: {
+    mayusculas(value) {
+      if (!value) return "";
+      return value.toUpperCase();
+    },
+    doble(num) {
+      return num * 2;
+    },
   },
   methods: {
     incrementarContadorPadre() {

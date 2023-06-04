15c9513598140a90e13cc7f3289df24bd0ebc2d4
Edgardo Gabriel Aguirre <aguirre5411@gmail.com>
2023-06-04 11:07:55 -0300

    Se agrega ejercicio de formulario

---
+++ clase08/aplicacion/src/components/Ejercicio.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form @submit.prevent="login">
+      <label for="username">Username</label>
+      <input type="text" id="username" v-model="username" />
+      <br />
+      <label for="password">Password</label>
+      <input type="password" id="password" v-model="password" />
+      <br />
+      <button type="submit">Login</button>
+    </form>
+    <p>{{ loginMessage }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      loginMessage: "",
+    };
+  },
+  methods: {
+    login() {
+      // Aquí puedes realizar la lógica de autenticación, como enviar una solicitud al servidor o verificar los datos localmente.
+      // En este ejemplo, simplemente mostraremos un mensaje de bienvenida con los datos del usuario.
+      this.loginMessage = `¡Bienvenido, ${this.username}! Tu contraseña es ${this.password}.`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Estilos del componente */
+</style>
